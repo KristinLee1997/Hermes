b07f43728819b6fc36facee3bb0371ad1c43ae56
Kristin <kristinleejava@gmail.com>
2019-04-27 19:24:14 +0800

    :sparkles: add pojo

--- hermes-dal/src/main/java/com/aries/hermes/dal/po/Category.java
+++ hermes-dal/src/main/java/com/aries/hermes/dal/po/Category.java
@@ -1,22 +1,41 @@
 package com.aries.hermes.dal.po;
 
+import javax.persistence.*;
+
+@Table(name = "`category`")
 public class Category {
+    @Id
+    @Column(name = "`id`")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "`name`")
     private String name;
 
+    /**
+     * @return id
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * @param id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * @return name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @param name
+     */
     public void setName(String name) {
         this.name = name == null ? null : name.trim();
     }
