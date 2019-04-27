b82bdcf7d55a9c36ee7b3bbf83735ad00538794b
arowana <arowana.java@qq.com>
2019-04-27 16:47:40 +0800

    :sparkles: test

--- hermes-dal/src/main/java/com/aries/hermes/dal/mapper/CompanyMapper.java
+++ hermes-dal/src/main/java/com/aries/hermes/dal/mapper/CompanyMapper.java
@@ -2,8 +2,7 @@ package com.aries.hermes.dal.mapper;
 
 import com.aries.hermes.dal.po.Company;
 import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
 
-public interface CompanyMapper extends Mapper<Company>, MySqlMapper {
+public interface CompanyMapper extends Mapper<Company> {
 //    Stu getStuById(int id);
 }
--- hermes-dal/src/main/java/com/aries/hermes/dal/repository/CompanyRepository.java
+++ hermes-dal/src/main/java/com/aries/hermes/dal/repository/CompanyRepository.java
@@ -1,7 +1,6 @@
 package com.aries.hermes.dal.repository;
 
 import com.aries.hermes.dal.mapper.CompanyMapper;
-import com.aries.hermes.dal.po.CompanyExample;
 import com.aries.hermes.dal.util.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 
@@ -11,12 +10,8 @@ public class CompanyRepository {
             // 获取Mapper
             CompanyMapper companyMapper = session.getMapper(CompanyMapper.class);
 
-            // 构建example
-            CompanyExample example = new CompanyExample();
-            example.createCriteria().andIdEqualTo(id);
-
             // 输出结果
-            System.out.println(companyMapper.selectOneByExample(example).getCompanyName());
+            System.out.println(companyMapper.selectByPrimaryKey(id).getCompanyName());
         }
         return true;
     }
