5891bf5da21be1a3d15888719ecb772ecb1460fa
arowana <arowana.java@qq.com>
2019-04-27 15:58:59 +0800

    :memo:

---
+++ hermes-dal/src/main/java/com/aries/hermes/dal/mapper/CompanyMapper.java
@@ -0,0 +1,8 @@
+package com.aries.hermes.dal.mapper;
+
+import com.aries.hermes.dal.po.Company;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CompanyMapper extends Mapper<Company> {
+//    Stu getStuById(int id);
+}
