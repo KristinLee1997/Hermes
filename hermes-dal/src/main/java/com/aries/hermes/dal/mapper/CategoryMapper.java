16c1a852b4dc4d6089e4d37c8ab57178419be21f
arowana <arowana.java@qq.com>
2019-04-27 19:38:16 +0800

    :sparkles: 新增category dao

---
+++ hermes-dal/src/main/java/com/aries/hermes/dal/mapper/CategoryMapper.java
@@ -0,0 +1,9 @@
+package com.aries.hermes.dal.mapper;
+
+import com.aries.hermes.dal.po.Category;
+import com.aries.hermes.dal.po.Company;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CategoryMapper extends Mapper<Category> {
+//    Stu getStuById(int id);
+}
