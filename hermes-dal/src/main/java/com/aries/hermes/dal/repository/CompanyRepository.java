6020554b2062896a4b6fbf80d4d406d2faaadf48
Kristin <kristinleejava@gmail.com>
2019-04-27 19:10:14 +0800

    :sparkles: insert company dao

--- hermes-dal/src/main/java/com/aries/hermes/dal/repository/CompanyRepository.java
+++ hermes-dal/src/main/java/com/aries/hermes/dal/repository/CompanyRepository.java
@@ -1,22 +1,37 @@
 package com.aries.hermes.dal.repository;
 
 import com.aries.hermes.dal.mapper.CompanyMapper;
+import com.aries.hermes.dal.po.Company;
+import com.aries.hermes.dal.po.CompanyExample;
 import com.aries.hermes.dal.util.SqlSessionUtil;
 import org.apache.ibatis.session.SqlSession;
 
-public class CompanyRepository {
-    public static boolean addCompany(String database, long id) {
-        try (SqlSession session = SqlSessionUtil.openSession(database)) {
-            // 获取Mapper
-            CompanyMapper companyMapper = session.getMapper(CompanyMapper.class);
+import java.util.List;
 
-            // 输出结果
-            System.out.println(companyMapper.selectByPrimaryKey(id).getCompanyName());
+public class CompanyRepository {
+    public static boolean addCompany(Company company) {
+        try (SqlSession session = SqlSessionUtil.openSession(company.getCompanyName())) {
+            CompanyMapper companyMapper = session.getMapper(CompanyMapper.class);
+            int i = companyMapper.insertSelective(company);
+            if (i > 0) {
+                return true;
+            } else {
+                return false;
+            }
         }
-        return true;
     }
 
-    public static void main(String[] args) {
-        System.out.println(addCompany("hermes", 1L));
+    public static Company selectByNameAndPassword(String companyName, String password) {
+        try (SqlSession session = SqlSessionUtil.openSession(companyName)) {
+            CompanyMapper companyMapper = session.getMapper(CompanyMapper.class);
+            CompanyExample example = new CompanyExample();
+            example.createCriteria().andCompanyNameEqualTo(companyName).andPasswordEqualTo(password);
+            List<Company> companyList = companyMapper.selectByExample(example);
+            if (companyList != null && companyList.size() > 0) {
+                return companyList.get(0);
+            } else {
+                return null;
+            }
+        }
     }
 }
--- hermes-dal/src/main/java/com/aries/hermes/dal/util/SqlSessionUtil.java
+++ hermes-dal/src/main/java/com/aries/hermes/dal/util/SqlSessionUtil.java
@@ -1,11 +1,17 @@
 package com.aries.hermes.dal.util;
 
+import com.aries.hermes.dal.constants.SystemConstants;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
 public class SqlSessionUtil {
     public static SqlSession openSession(String databaseName) {
-        SqlSessionFactory sqlSessionFactory = SqlSessionFactoryUtil.getSqlSessionFactory(databaseName);
+        SqlSessionFactory sqlSessionFactory = null;
+        if (databaseName.equals(SystemConstants.SYSTEM_DATABASE_NAME)) {
+            sqlSessionFactory = SqlSessionFactoryUtil.getSqlSessionFactory(databaseName);
+        } else {
+            sqlSessionFactory = SqlSessionFactoryUtil.getSqlSessionFactory(SystemConstants.DATABASE_NAME_PREFIX + databaseName);
+        }
         return sqlSessionFactory.openSession(true);
     }
 }
