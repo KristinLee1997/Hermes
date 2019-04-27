e5b23b624f76fa7eec972018a8350bd92cd41474
Kristin <kristinleejava@gmail.com>
2019-04-27 13:24:17 +0800

    :tada: 新增module

---
+++ hermes-core/src/main/java/com/aries/hermes/core/App.java
@@ -0,0 +1,10 @@
+package com.aries.hermes.core;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ hermes-starter/src/test/java/com/aries/hermes/starter/AppTest.java
@@ -0,0 +1,18 @@
+package com.aries.hermes.starter;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
