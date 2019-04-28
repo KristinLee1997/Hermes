e389f792dcc109718bace4fdc4b227eb99950f69
arowana <arowana.java@qq.com>
2019-04-28 16:53:05 +0800

    :sparkles: 添加 CompanyHelper

--- hermes-server-thrift/src/main/java/com/aries/hermes/server/thrift/server/TopicServerImpl.java
+++ hermes-server-thrift/src/main/java/com/aries/hermes/server/thrift/server/TopicServerImpl.java
@@ -1,12 +1,50 @@
 package com.aries.hermes.server.thrift.server;
 
-import com.aries.hermes.idl.dto.ResponseCode;
+import com.aries.hermes.idl.dto.CompanyDTO;
+import com.aries.hermes.idl.dto.ReplyDTO;
+import com.aries.hermes.idl.dto.ThriftResponse;
 import com.aries.hermes.idl.service.TopicServer;
+import com.aries.hermes.server.thrift.server.util.CompanyHelper;
 import org.apache.thrift.TException;
+
+import java.util.Collections;
+import java.util.List;
 
 public class TopicServerImpl implements TopicServer.Iface {
     @Override
     public String ping() throws TException {
+
         return "pong,this is topic server";
+    }
+
+    public ThriftResponse xxx(CompanyDTO companyDTO) {
+        CompanyHelper companyHelper = new CompanyHelper(companyDTO).invoke();
+        if (companyHelper.isError()) {
+            return companyHelper.getResponse();
+        }
+
+
+        return null;
+    }
+
+
+    public List xxxx(CompanyDTO companyDTO) {
+        CompanyHelper companyHelper = new CompanyHelper(companyDTO).invoke();
+        if (companyHelper.isError()) {
+            return Collections.emptyList();
+        }
+
+
+        return null;
+    }
+
+    public ReplyDTO xxxx(CompanyDTO companyDTO) {
+        CompanyHelper companyHelper = new CompanyHelper(companyDTO).invoke();
+        if (companyHelper.isError()) {
+            return null;
+        }
+
+
+        return null;
     }
 }
---
+++ hermes-server-thrift/src/main/java/com/aries/hermes/server/thrift/server/util/CompanyHelper.java
@@ -0,0 +1,34 @@
+package com.aries.hermes.server.thrift.server.util;
+
+import com.aries.hermes.idl.dto.CompanyDTO;
+import com.aries.hermes.idl.dto.ThriftResponse;
+import com.aries.hermes.server.thrift.server.bean.CompanyBean;
+import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+
+
+@Getter
+public class CompanyHelper {
+    private boolean error;
+    private CompanyDTO companyDTO;
+    private String databaseName;
+    private ThriftResponse response;
+
+    public CompanyHelper(CompanyDTO companyDTO) {
+        this.companyDTO = companyDTO;
+    }
+
+    public CompanyHelper invoke() {
+        CompanyBean companyBean = new CompanyBean(companyDTO.getName(), companyDTO.getPassword(), companyDTO.getKeyword());
+        databaseName = PartnerBll.getDatabase(companyBean);
+
+        if (StringUtils.isBlank(databaseName)) {
+            response = PERMISSION_FAIL.of();
+            error = true;
+            return this;
+        }
+
+        error = false;
+        return this;
+    }
+}
